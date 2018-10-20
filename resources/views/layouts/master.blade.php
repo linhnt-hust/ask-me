a82d1c6caa401602096858c65e2a62590c9617bd
linhntHust <chaosmeteor96@gmail.com>
2018-10-20 16:21:42 +0700

    add master layouts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="ie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs -->
+	<meta charset="utf-8">
+	<title> @yield('title') </title>
+	<meta name="description" content="Ask me Responsive Questions and Answers Template">
+	<meta name="author" content="vbegy">
+	
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Main Style -->
+	<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+	
+	<!-- Skins -->
+	<link rel="stylesheet" href="{{ asset('/css/skins/blue.css') }}">
+	
+	<!-- Responsive Style -->
+	<link rel="stylesheet" href="{{ asset('/css/responsive.css') }}">
+	
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="{{ asset('/images/favicon.png') }}">
+  
+</head>
+<body>
+
+<div class="loader"><div class="loader_html"></div></div>
+
+<div id="wrap" class="grid_1200">
+	
+	<div class="login-panel">
+		<section class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="page-content">
+						<h2>Login</h2>
+						<div class="form-style form-style-3">
+							<form>
+								<div class="form-inputs clearfix">
+									<p class="login-text">
+										<input type="text" value="Username" onfocus="if (this.value == 'Username') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Username';}">
+										<i class="icon-user"></i>
+									</p>
+									<p class="login-password">
+										<input type="password" value="Password" onfocus="if (this.value == 'Password') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Password';}">
+										<i class="icon-lock"></i>
+										<a href="#">Forget</a>
+									</p>
+								</div>
+								<p class="form-submit login-submit">
+									<input type="submit" value="Log in" class="button color small login-submit submit">
+								</p>
+								<div class="rememberme">
+									<label><input type="checkbox" checked="checked"> Remember Me</label>
+								</div>
+							</form>
+						</div>
+					</div><!-- End page-content -->
+				</div><!-- End col-md-6 -->
+				<div class="col-md-6">
+					<div class="page-content Register">
+						<h2>Register Now</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi adipiscing gravdio, sit amet suscipit risus ultrices eu. Fusce viverra neque at purus laoreet consequa. Vivamus vulputate posuere nisl quis consequat.</p>
+						<a class="button color small signup">Create an account</a>
+					</div><!-- End page-content -->
+				</div><!-- End col-md-6 -->
+			</div>
+		</section>
+	</div><!-- End login-panel -->
+	
+	<div class="panel-pop" id="signup">
+		<h2>Register Now<i class="icon-remove"></i></h2>
+		<div class="form-style form-style-3">
+			<form>
+				<div class="form-inputs clearfix">
+					<p>
+						<label class="required">Username<span>*</span></label>
+						<input type="text">
+					</p>
+					<p>
+						<label class="required">E-Mail<span>*</span></label>
+						<input type="email">
+					</p>
+					<p>
+						<label class="required">Password<span>*</span></label>
+						<input type="password" value="">
+					</p>
+					<p>
+						<label class="required">Confirm Password<span>*</span></label>
+						<input type="password" value="">
+					</p>
+				</div>
+				<p class="form-submit">
+					<input type="submit" value="Signup" class="button color small submit">
+				</p>
+			</form>
+		</div>
+	</div><!-- End signup -->
+	
+	<div class="panel-pop" id="lost-password">
+		<h2>Lost Password<i class="icon-remove"></i></h2>
+		<div class="form-style form-style-3">
+			<p>Lost your password? Please enter your username and email address. You will receive a link to create a new password via email.</p>
+			<form>
+				<div class="form-inputs clearfix">
+					<p>
+						<label class="required">Username<span>*</span></label>
+						<input type="text">
+					</p>
+					<p>
+						<label class="required">E-Mail<span>*</span></label>
+						<input type="email">
+					</p>
+				</div>
+				<p class="form-submit">
+					<input type="submit" value="Reset" class="button color small submit">
+				</p>
+			</form>
+			<div class="clearfix"></div>
+		</div>
+	</div><!-- End lost-password -->
+	
+	@include('layouts.header_top')
+
+	@include('layouts.header')
+
+	<!-- Content -->
+	@yield('content')
+	<!-- End Content -->
+
+	@include('layouts.footer')
+
+	@include('layouts.footer_bottom')
+</div><!-- End wrap -->
+
+<div class="go-up"><i class="icon-chevron-up"></i></div>
+
+@include('layouts.bottom_js')
+
+</body>
+</html>
