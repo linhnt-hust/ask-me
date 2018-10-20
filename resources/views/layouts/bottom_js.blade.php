e8236750c208cde304f1ac0a5b3a3d4c48ad54f4
linhntHust <chaosmeteor96@gmail.com>
2018-10-20 16:21:42 +0700

    add master layouts

---
+++ resources/views/layouts/bottom_js.blade.php
@@ -0,0 +1,19 @@
+<!-- js -->
+<script src="{{ asset('/js/jquery.min.js') }}"></script>
+<script src="{{ asset('/js/jquery-ui-1.10.3.custom.min.js') }}"></script>
+<script src="{{ asset('/js/jquery.easing.1.3.min.js') }}"></script>
+<script src="{{ asset('/js/html5.js') }}"></script>
+<script src="{{ asset('/js/twitter/jquery.tweet.js') }}"></script>
+<script src="{{ asset('/js/jflickrfeed.min.js') }}"></script>
+<script src="{{ asset('/js/jquery.inview.min.js') }}"></script>
+<script src="{{ asset('/js/jquery.tipsy.js') }}"></script>
+<script src="{{ asset('/js/tabs.js') }}"></script>
+<script src="{{ asset('/js/jquery.flexslider.js') }}"></script>
+<script src="{{ asset('/js/jquery.prettyPhoto.js') }}"></script>
+<script src="{{ asset('/js/jquery.carouFredSel-6.2.1-packed.js') }}"></script>
+<script src="{{ asset('/js/jquery.scrollTo.js') }}"></script>
+<script src="{{ asset('/js/jquery.nav.js') }}"></script>
+<script src="{{ asset('/js/tags.js') }}"></script>
+<script src="{{ asset('/js/jquery.bxslider.min.js') }}"></script>
+<script src="{{ asset('/js/custom.js') }} "></script>
+<!-- End js -->
